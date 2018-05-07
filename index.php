f8c56b1ef66f9bcc93bb6995bc306f6b93eabee2
ClementAh <clement.allalah@gmail.com>
2018-05-07 17:18:44 +0200

    Integration first page MEGASKY

---
+++ index.php
@@ -0,0 +1,407 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <title>MegaSky</title>
+</head>
+<body>
+
+<!-- Navbar -->
+
+<nav class="navbar navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <img class="logo" src="./assets/img/megasky.png">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navlink navbar collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item ">
+                <a class="nav-link" href="#">BANDA LARGA</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">SKY PRE PAGO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">SKY POS PAGO</a>
+            </li>
+        </ul>
+
+        <a class="btn btn-primary" href="#"> ASSINE JA</a>
+    </div>
+</nav>
+
+<!-- Slider -->
+
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="./assets/img/sky.png" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/img/sky.png" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="./assets/img/sky.png" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+</div>
+
+
+<!-- Slider TV -->
+
+
+<div id="TvExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#TvExampleIndicators" data-slide-to="0" class="active" style="background-color: #FD4A4A"></li>
+        <li data-target="#TvExampleIndicators" data-slide-to="1"></li>
+
+    </ol>
+
+    <!-- First slide -->
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="card" style="width: 18rem;">
+                <div class="categorie">
+                    <p>SMART</p>
+                </div>
+                <img class="card-img-top" src="./assets/img/hunger.jpg" alt="Card image cap">
+                <div class="description container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="canais col-md-3">
+                                        <p><span>103</span></p>
+                                        <p>Canais</p>
+                                    </div>
+                                    <div class="canais col-md-3">
+                                        <p><span>0</span></p>
+                                        <p>Canais</p>
+                                    </div>
+                                    <div class="canais col-md-3">
+                                        <p><span>1</span></p>
+                                        <p>Equipamento</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="price">
+                    <p><span>R$32,45</span></p>
+                    <p>NO PRIMERO MES</p>
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="btn1 col-md-6">
+                            <a class=" btn-primary" href="#">DETALHES</a>
+                        </div>
+                        <div class="btn2 col-md-6">
+                            <a class=" btn-primary" href="#">ASSINAR</a>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="card" style="width: 18rem;">
+
+            <div class="categorie">
+                <p>SMART HD</p>
+            </div>
+            <img class="card-img-top" src="./assets/img/spider.jpg" alt="Card image cap">
+            <div class="card-body">
+
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <div class="categorie">
+                <p>SMART II HD</p>
+            </div>
+            <img class="card-img-top" src="./assets/img/hunger.jpg" alt="Card image cap">
+            <div class="card-body">
+
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Second slide -->
+
+    <div class="carousel-item">
+        <div class="carousel-item active">
+            <div class="card" style="width: 18rem;">
+                <div class="categorie">
+                    <p>SMART II HD</p>
+                </div>
+                <img class="card-img-top" src="./assets/img/hunger.jpg" alt="Card image cap">
+                <div class="card-body">
+
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="categorie">
+                    <p>SMART II HD</p>
+                </div>
+                <img class="card-img-top" src="./assets/img/spider.jpg" alt="Card image cap">
+                <div class="card-body">
+
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="categorie">
+                    <p>SMART II HD</p>
+                </div>
+                <img class="card-img-top" src="./assets/img/hunger.jpg" alt="Card image cap">
+                <div class="card-body">
+
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!--        <a class="carousel-control-prev" href="#TvExampleIndicators" role="button" data-slide="prev">-->
+    <!--            <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+    <!--            <span class="sr-only">Previous</span>-->
+    <!--        </a>-->
+    <!--        <a class="carousel-control-next" href="#TvExampleIndicators" role="button" data-slide="next" style="color: #FD4A4A">-->
+    <!--            <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+    <!--            <span class="sr-only">Next</span>-->
+    <!--        </a>-->
+</div>
+</div>
+
+
+
+<!-- Banda larga slider -->
+
+
+<div id="BandaExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#BandaExampleIndicators" data-slide-to="0" class="active" style="background-color: #FD4A4A"></li>
+        <li data-target="#BandaExampleIndicators" data-slide-to="1"></li>
+
+    </ol>
+
+    <!-- First slide -->
+    <div class="Banda carousel-inner">
+        <div class="carousel-item active">
+            <div class="card" style="width: 18rem;">
+                <div class="titleCat">
+                    <p>Light + WI-FI</p>
+                </div>
+
+                <div class="card-body">
+
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+
+                <div class="titleCat">
+                    <p>4 MEGA + WI-FI</p>
+                </div>
+
+                <div class="card-body">
+
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+
+                <div class="titleCat">
+                    <p>2 MEGA + WI-FI</p>
+                </div>
+
+                <div class="card-body">
+
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Second slide -->
+
+        <div class="carousel-item">
+            <div class="carousel-item active">
+                <div class="card" style="width: 18rem;">
+                    <div class="titleCat">
+                        <p>Light + WI-FI</p>
+                    </div>
+
+                    <div class="card-body">
+                        
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <div class="titleCat">
+                        <p>Light + WI-FI</p>
+                    </div>
+
+                    <div class="card-body">
+
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <div class="titleCat">
+                        <p>Light + WI-FI</p>
+                    </div>
+
+                    <div class="card-body">
+
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#BandaExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </a>
+        <a class="carousel-control-next" href="#BandaExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+
+
+<div class="spotlight container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <img class="logoSky" src="./assets/img/logoSky.png">
+        </div>
+        <div class="spots col-md-4">
+            <div class="container">
+                <div class="row">
+                    <div class="link col-md-12">
+                        <a class="link2" href="#">SKY HD + BANDA LARGA</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-4">
+            <a class="SpotButton btn btn-primary">ASSINE JA</a>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="sectionContact container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="container">
+                <div class="row">
+                    <div class="imgContact col-md-12">
+                        <img class="contact" src="./assets/img/contact.png">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-6">
+            <div class="container">
+                <div class="row">
+                    <div class="form col-md-6">
+                        <form method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Nome" name="name">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control"  placeholder="Email" name="mail">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Telephone" name="telephone">
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>BANDA LARGA</option>
+                                    <option>PRE PAGO</option>
+                                    <option>POS PAGO</option>
+
+                                </select>
+                            </div>
+                            <div class="form-group">
+
+                                <textarea class="form-control" placeholder="Message" rows="5"></textarea>
+                            </div>
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="container">
+
+</div>
+
+<div class="footer container-fluid">
+    <div class="row">
+        <div class="logo col-md-3">
+            <img class="imgfooter" src="./assets/img/megaFooter.png">
+        </div>
+    </div>
+
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</body>
+</html>
